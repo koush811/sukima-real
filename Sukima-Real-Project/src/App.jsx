e6c214c39d94268979d46b48727b145fc97f3d08
kosei <123241044+koush811@users.noreply.github.com>
2026-07-20 21:10:03 +0900

    login保持

--- Sukima-Real-Project/src/App.jsx
+++ Sukima-Real-Project/src/App.jsx
@@ -6,14 +6,48 @@ import Schedule from "./pages/Schedule";
 import Photo from "./pages/Photo";
 import Detail from "./pages/Detail";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/photo" element={<Photo />} />
-      <Route path="/detail/:id" element={<Detail />} />
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/schedule"
+        element={
+          <ProtectedRoute>
+            <Schedule />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/photo"
+        element={
+          <ProtectedRoute>
+            <Photo />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/detail/:id"
+        element={
+          <ProtectedRoute>
+            <Detail />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
--- Sukima-Real-Project/src/pages/Login.jsx
+++ Sukima-Real-Project/src/pages/Login.jsx
@@ -2,8 +2,19 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, provider, db } from "../firebase/firebase";
 import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
+
+  const { user } = useAuth();
+
+    useEffect(() => {
+    if (user) {
+        navigate("/home");
+    }
+    }, [user]);
+
   const navigate = useNavigate();
 
   async function GoogleLogin() {
